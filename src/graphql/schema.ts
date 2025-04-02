7609210416501cfcd257c71ecb232a9fdd26aa70
ryo.ito <r.ito@salescore.jp>
2025-04-02 17:21:02 +0900

    nexusを使った実装に更新

---
+++ src/graphql/schema.ts
@@ -0,0 +1,86 @@
+import { booleanArg, makeSchema, mutationType, nonNull, objectType, queryType, stringArg } from "nexus";
+import path from "path";
+
+const Query = queryType({
+  definition(t) {
+    t.nonNull.list.field("getTodos", {
+      description: "TODO一覧取得",
+      type: Todo,
+      resolve(_parent, _args, context) {
+        return context.prisma.todo.findMany();
+      }
+    })
+  },
+});
+
+const Mutation = mutationType({
+  definition(t) {
+    t.field("addTodo", {
+      description: "TODO追加",
+      args: { title: nonNull(stringArg()) },
+      type: "Todo",
+      resolve(_parent, args, context) {
+        return context.prisma.todo.create({
+          data: {
+            title: args.title,
+            completed: false,
+          },
+        });
+      }
+    })
+    t.field("updateTodo", {
+      description: "TODO更新",
+      args: {
+        id: nonNull(stringArg()),
+        completed: nonNull(booleanArg()),
+      },
+      type: "Todo",
+      resolve(_parent, args, context) {
+        return context.prisma.todo.update({
+          where: { id: args.id },
+          data: { completed: args.completed },
+        });
+      }
+    })
+    t.field("deleteTodo", {
+      description: "TODO削除",
+      args: { id: nonNull(stringArg()) },
+      type: "Todo",
+      resolve(_parent, args, context) {
+        return context.prisma.todo.delete({
+          where: { id: args.id },
+        });
+      }
+    })
+  }
+})
+
+const Todo = objectType({
+  name: "Todo",
+  description: "Todoの型定義",
+  definition(t) {
+    t.nonNull.id("id", { description: "ID" });
+    t.nonNull.string("title", { description: "タイトル" });
+    t.nonNull.boolean("completed", { description: "完了フラグ" });
+  }
+})
+
+export const schema = makeSchema({
+  types: [Query, Mutation, Todo],
+  outputs: {
+    typegen: path.join(process.cwd(), "generated", "nexus-typegen.ts"),
+    schema: path.join(process.cwd(), "generated", "schema.graphql"),
+  },
+  contextType: {
+    module: path.join(process.cwd(), "src", "graphql", "context.ts"),
+    export: "Context",
+  },
+  sourceTypes: {
+    modules: [
+      {
+        module: "@prisma/client",
+        alias: "prisma",
+      },
+    ],
+  },
+});
