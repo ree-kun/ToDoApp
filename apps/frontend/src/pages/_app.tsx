df303091368b393732c99be73e90f7c8a63cc140
ryo.ito <r.ito@salescore.jp>
2025-04-08 15:34:54 +0900

    storeを設定

--- apps/frontend/src/pages/_app.tsx
+++ apps/frontend/src/pages/_app.tsx
@@ -1,15 +1,36 @@
 import "@/styles/globals.css";
 import "@/styles/tailwind.css";
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import { configureStore, createSlice } from "@reduxjs/toolkit";
 import type { AppProps } from "next/app";
+import { Provider } from "react-redux";
 
 const client = new ApolloClient({
   uri: "http://localhost:4000",
   cache: new InMemoryCache(),
 });
 
+const titleSlice = createSlice({
+  name: 'title',
+  initialState: {
+    value: ""
+  },
+  reducers: {
+    titleUpdate: (state, action) => {
+      state.value = action.payload
+    },
+  },
+})
+
+const store = configureStore({
+  reducer: titleSlice.reducer
+})
 export default function App({ Component, pageProps }: AppProps) {
-  return <ApolloProvider client={client}>
-    <Component {...pageProps} />
-  </ApolloProvider>;
+  return (
+    <Provider store={store}>
+      <ApolloProvider client={client}>
+        <Component {...pageProps} />
+      </ApolloProvider>
+    </Provider>
+  );
 }
